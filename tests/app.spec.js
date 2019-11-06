186decdf8ae37af33630719455e14828a1dc4d6b
ian douglas <ian.douglas@iandouglas.com>
2019-11-06 14:44:35 -0700

    updating app.spec.js to test travis

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,8 +4,9 @@ var app = require('../app');
 
 describe('Test the root path', () => {
   test('It should respond to the GET method', () => {
-    return request(app).get("/").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
+    const res = await request(app)
+      .get("/");
+
+    expect(res.statusCode).toBe(200);
   });
 });
