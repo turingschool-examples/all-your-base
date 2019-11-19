86a062d33ddea6a080fa4d815f4454d936b2726b
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-18 20:05:23 -0700

    Update production db connection

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: "",
+    connection:
+      "postgres://sqlwyyedqfrlmf:55ae411fcc746cdce5c9c08a2e79bd19c2632eacc280591d0c6b9eba14373c3f@ec2-174-129-253-174.compute-1.amazonaws.com:5432/d32561anrjh13j",
     migrations: {
       directory: "./db/migrations"
     },
