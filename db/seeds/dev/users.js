89da5a89da296262ac4f67f43c5d8841d4db5d7a
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-18 21:09:22 -0700

    Create users seeds file

---
+++ db/seeds/dev/users.js
@@ -0,0 +1,23 @@
+exports.seed = function(knex) {
+  // We must return a Promise from within our seed function
+  // Without this initial `return` statement, the seed execution
+  // will end before the asynchronous tasks have completed
+  return knex('users').del() // delete all users
+
+    // Now that we have a clean slate, we can re-insert our paper data
+    .then(() => {
+      return Promise.all([
+
+        // Insert a single paper, return the paper ID, insert 2 footnotes
+        knex('users').insert({
+          api_key: '878uahfa8y89aydf89yas98fyas9h'
+        }, 'id')
+        .insert({
+          api_key: '2398qpncq28nrc8q7r987cq2987q8q'
+        }, 'id')
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ]) // end return Promise.all
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
