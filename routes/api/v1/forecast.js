13e8dcb30c800c3fb5d9ae0d3f10a1471548313c
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-19 22:38:37 -0700

    Delete commented out code and change variable name for json response to camel case

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,63 +1,47 @@
 require('dotenv').config();
 var express = require('express');
 var router = express.Router();
-// var request = require('request');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const rp = require('request-promise');
 
-
-
- // rp('http://www.google.com').then(body => {
- //     console.log(body);
- // }).catch(err => {
- //     console.log(err);
- // });
-
 router.get('/', (request, response) => {
   var options = {
       uri: 'https://maps.googleapis.com/maps/api/geocode/json',
       qs: {
-          key: process.env.GOOGLE_API_KEY, // -> uri + '?access_token=xxxxx%20xxxxx'
+          key: process.env.GOOGLE_API_KEY,
           address: request.query.location
       },
-      json: true // Automatically parses the JSON string in the response
+      json: true
   };
 
-  // console.log(process.env.GOOGLE_API_KEY)
-    // console.log(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}&address=${request.query.location}`)
-  // request(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}&address=${request.query.location}`, function (error, response, body) {
-  // if (!error && response.statusCode == 200) {
-  //   console.log(body) // Show the HTML for the Google homepage.
-  // }
-// }); [1].geometry.location.lat
 rp(options).then(body => {
     var latitude = body.results[0].geometry.location.lat;
     var longitude = body.results[0].geometry.location.lng;
 
     var options2 = {
         uri: `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${latitude},${longitude}?exclude=minutely`,
-        json: true // Automatically parses the JSON string in the response
+        json: true
     };
 
     rp(options2).then(body2 => {
-      var json_response = {} // empty Object
-      json_response["location"] = request.query.location; // empty Array, which you can push() values into
-      json_response["currently"] = body2.currently
-      json_response["hourly"] = body2.hourly
-      json_response["daily"] = body2.daily
+      var jsonResponse = {}
+      jsonResponse["location"] = request.query.location;
+      jsonResponse["currently"] = body2.currently
+      jsonResponse["hourly"] = body2.hourly
+      jsonResponse["daily"] = body2.daily
 
-      delete json_response.currently.time
-      delete json_response.currently.nearestStormDistance
-      delete json_response.currently.nearestStormBearing
-      delete json_response.currently.apparentTemperature
-      delete json_response.currently.dewPoint
-      delete json_response.currently.uvIndex
-      delete json_response.currently.ozone
+      delete jsonResponse.currently.time
+      delete jsonResponse.currently.nearestStormDistance
+      delete jsonResponse.currently.nearestStormBearing
+      delete jsonResponse.currently.apparentTemperature
+      delete jsonResponse.currently.dewPoint
+      delete jsonResponse.currently.uvIndex
+      delete jsonResponse.currently.ozone
 
-      json_response.hourly.data.forEach((hash) => {
+      jsonResponse.hourly.data.forEach((hash) => {
         delete hash.precipType;
         delete hash.apparentTemperature;
         delete hash.dewPoint;
@@ -65,7 +49,7 @@ rp(options).then(body => {
         delete hash.ozone;
       });
 
-      json_response.daily.data.forEach((hash) => {
+      jsonResponse.daily.data.forEach((hash) => {
         delete hash.moonPhase;
         delete hash.temperatureHighTime;
         delete hash.temperatureLowTime;
@@ -87,7 +71,7 @@ rp(options).then(body => {
         delete hash.apparentTemperatureMinTime;
         delete hash.precipAccumulation;
       });
-        response.status(200).json(json_response)
+        response.status(200).json(jsonResponse)
     }).catch(err => {
         console.log(err);
     });
