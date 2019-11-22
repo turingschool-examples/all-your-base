df3526d3456890dbca6118851d3366de8d5a0fe8
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-21 17:08:47 -0700

    Update dev/test databases, add Heroku database and seeds to production

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/publications_dev',
+    connection: 'postgres://localhost/sweater_weather_2_dev',
     migrations: {
       directory: './db/migrations'
     },
@@ -15,7 +15,7 @@ module.exports = {
   },
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/publications_test',
+    connection: 'postgres://localhost/sweater_weather_2_test',
     migrations: {
       directory: './db/migrations'
     },
@@ -23,9 +23,12 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://txdvikuftgqoai:ddf2b51d20cff800b59fef49281a45c6e09fb7dcb66e4f6bdb417eb6b51b03c7@ec2-107-22-163-220.compute-1.amazonaws.com:5432/dbq2bfkr4ochpo',
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
