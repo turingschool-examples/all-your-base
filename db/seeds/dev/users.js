3e56981d064d635c7eba01724ecbce4c648c6df9
Brian Bower <brian@brianbower.com>
2020-01-27 14:43:49 -0700

    Setup Continued

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -1,20 +1,24 @@
+/*jshint esversion: 6 */
+
 exports.seed = function(knex) {
-  // We must return a Promise from within our seed function
-  // Without this initial `return` statement, the seed execution
-  // will end before the asynchronous tasks have completed
-  return knex('users').del() // delete all users
+ // Deletes ALL existing entries
+ return knex('favorites').del()
+   .then(() => knex('users').del())
 
-    // Now that we have a clean slate, we can re-insert our paper data
-    .then(() => {
-      return Promise.all([
-
-        // Insert a single paper, return the paper ID, insert 2 footnotes
-        knex('users').insert({
-          api_key: '123456789'
-        }, 'id')
-        .then(() => console.log('Seeding complete!'))
-        .catch(error => console.log(`Error seeding data: ${error}`))
-      ]) // end return Promise.all
-    })
-    .catch(error => console.log(`Error seeding data: ${error}`));
+     .then(() => {
+       return Promise.all([
+         knex('users').insert({
+           api_key: '123456789'
+         }, 'id')
+         .then(user => {
+           return knex('favorites').insert([
+             { location: 'Denver,CO', user_id: user[0] },
+             { location: 'Orlando,FL', user_id: user[0] }
+           ]);
+         })
+         .then(() => console.log('Seeding Complete!'))
+         .catch(error => console.log(`Error seeding data: ${error}`))
+       ]);
+     })
+     .catch(error => console.log(`Error seeding data: ${error}`));
 };
--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://rnnzoyjoynebtm:f852dac6a0d21c6108fa0dd3967fd4d956f1483d0d81823af9698d4bb3fa233e@ec2-3-220-86-239.compute-1.amazonaws.com:5432/d3ikhmvjh99jfo',
     migrations: {
       directory: './db/migrations'
     },
