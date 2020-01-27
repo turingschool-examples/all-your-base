ebab6002bd64e0c74cb07df90d95f33d6e38d047
Brian Bower <brian@brianbower.com>
2020-01-27 14:27:05 -0700

    Setup

--- db/migrations/20190128225204_initial.js
+++ db/migrations/20190128225204_initial.js
@@ -1,19 +1,31 @@
 
 exports.up = function(knex) {
   return Promise.all([
-    knex.schema.createTable('papers', function(table) {
+    knex.schema.createTable('users', function(table) {
       table.increments('id').primary();
-      table.string('title');
-      table.string('author');
+      table.string('api_key');
+
+      table.timestamps(true, true);
+    }),
+
+
+    knex.schema.createTable('favorites', function(table) {
+      table.increments('id').primary();
+      table.string('location');
+      table.integer('user_id').unsigned();
+      table.foreign('user_id')
+        .references('users.id');
 
       table.timestamps(true, true);
     })
-  ])
+  ]);
 };
 
 
-exports.down = function(knex) {
+exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('papers')
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('favorites')
   ]);
-}
+};
+
