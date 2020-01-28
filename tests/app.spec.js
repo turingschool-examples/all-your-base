24a1432db8a399c729fa930dbc70d86ad7ddd701
Mary Lang <mcl86@cornell.edu>
2020-01-28 16:27:21 -0700

    Add test for user endpoint

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -2,6 +2,11 @@ var shell = require('shelljs');
 var request = require("supertest");
 var app = require('../app');
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+
 describe('Test the root path', () => {
   test('It should respond to the GET method', async () => {
     const res = await request(app)
@@ -10,3 +15,35 @@ describe('Test the root path', () => {
     expect(res.statusCode).toBe(200);
   });
 });
+
+
+describe('users', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table users cascade');
+
+    let user = {
+      email: 'foobar@gmail.com', api_key: '123456897'
+    };
+    await database('users').insert(user, 'id')
+  });
+
+  afterEach(() => {
+    database.raw('truncate table users cascade')
+  });
+
+  describe('test users endpoint', () => {
+    it('tests happy path', async () => {
+      const res = await request(app)
+        .get('/api/v1/users');
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBe(1);
+
+      expect(res.body[0]).toHaveProperty('email');
+      expect(res.body[0].email).toBe('foozzzbar@gmail.com');
+
+      expect(res.body[0]).toHaveProperty('api_key');
+      expect(res.body[0].api_key).toBe('123456897');
+    })
+  })
+})
