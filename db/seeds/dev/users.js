61f60a97d74d842270f124ae3fc63538ed146893
Laura Schulz <lrs8810@gmail.com>
2020-01-27 18:18:32 -0700

    Add users seeds

---
+++ db/seeds/dev/users.js
@@ -0,0 +1,20 @@
+exports.seed = function(knex) {
+  // We must return a Promise from within our seed function
+  // Without this initial `return` statement, the seed execution
+  // will end before the asynchronous tasks have completed
+  return knex('users').del() // delete all papers
+
+    // Now that we have a clean slate, we can re-insert our paper data
+    .then(() => {
+      return Promise.all([
+
+        // Insert a single paper, return the paper ID, insert 2 footnotes
+        knex('users').insert({
+          email: 'lrs8810@gmail.com', api_key: 'jgn983hy48thw9begh98h4539h4'
+        }, 'id')
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ]) // end return Promise.all
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
