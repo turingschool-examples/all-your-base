52fcd02d052da2c8fe9ddc442e99bb8ddff30cf8
Mary Lang <mcl86@cornell.edu>
2020-01-28 16:26:53 -0700

    Remove id from seeds for users

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -7,9 +7,9 @@ exports.seed = function(knex) {
       return Promise.all([
 
         knex('users').insert([
-          {id: 1, api_key: '123456789', email: 'whereswaldo123@gmail.com' },
-          {id: 2, api_key: '987654321', email: 'carmensandiego456@gmail.com' },
-          {id: 3, api_key: '024681357', email: 'harrietthespy789@gmail.com' }
+          {api_key: '123456789', email: 'whereswaldo123@gmail.com' },
+          {api_key: '987654321', email: 'carmensandiego456@gmail.com' },
+          {api_key: '024681357', email: 'harrietthespy789@gmail.com' }
         ])
         .then(() => console.log('Seeding complete'))
         .catch(error => console.log(`Error seeding data: ${error}`))
