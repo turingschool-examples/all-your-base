fdc31a9e4ea14cd3c3421d190655ad1dbd82aefb
Mary Lang <mcl86@cornell.edu>
2020-01-27 18:59:11 -0700

    refactor routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const users = require('./routes/api/v1/users')
+const app = express();
+const forecast = require('./routes/api/v1/forecasts')
+const favorites = require('./routes/api/v1/favorites')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+app.use('/api/v1/users', users)
+
+app.use('/api/v1/forecast', forecast)
+
+app.use('/api/v1/favorites', favorites)
+
 
 module.exports = router;
