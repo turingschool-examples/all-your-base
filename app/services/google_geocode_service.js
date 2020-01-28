8a4aa0027231a1c088e737e1d4bace6f18662aea
Mary Lang <mcl86@cornell.edu>
2020-01-28 16:28:06 -0700

    Have services broken out to potentially use

--- app/services/google_geocode_service.js
+++ app/services/google_geocode_service.js
@@ -1,4 +1,22 @@
+const request = require('request')
+require('dotenv').config({'/.env'})
+
 class GoogleGeocodeService {
 
-  
+  static getLocation(location){
+    getJSON(location)
+  }
+
+  static connection(location) {
+    request(`https://maps.googleapis.com/maps/api/geocode/json`, { json: true }, (err, res, body) => {
+      if (err) { return console.log(err); }
+      console.log(body.url);
+      console.log(body.explanation);
+    });
+  }
+
+  static getJSON(location) {
+    response = connection(location)
+    JSON.parse(response.body, symbolize_names: true)
+  }
 }
