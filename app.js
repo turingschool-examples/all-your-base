050b2f8d698ba7d135c2a2079d6f38f030c8d3bf
Jon Patterson <JonPatt92@gmail.com>
2020-01-30 09:40:21 -0700

    Add forecastRouter and require google service file

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration)
 
 var indexRouter = require('./routes/index');
-var papersRouter = require('./routes/api/v1/papers');
+var forecastRouter = require('./routes/api/v1/forecast');
 
 var app = express();
 
@@ -16,8 +17,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'sweater-weather';
 
 app.use('/', indexRouter);
-app.use('/api/v1/papers', papersRouter);
+app.use('/api/v1/forecast', forecastRouter);
 
-module.exports = app;
+require('./lib/google')
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
