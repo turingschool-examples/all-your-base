ac3bfb11544cd67f35b5eeff2b365cf9ea8c4583
Brian Bower <brian@brianbower.com>
2020-01-29 23:01:41 -0700

    Add favorites endpoint router which allows a user to post a new favorite location to the database

---
+++ routes/api/v1/favorites.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+const fetch = require('node-fetch');
+
+router.post('/', (request, response) => {
+    if (request.body.api_key) {
+    database('users').where('api_key', request.body.api_key).first()
+      .then((user) => {
+        if (user && request.body.location) {
+
+          let favoriteLocation = request.body.location;
+
+          database('favorites').insert([
+            { location: favoriteLocation, user_id: user.id },
+          ])
+            .then(favoriteLocation => {
+              console.log(favoriteLocation);
+              response.status(201).json({ "message": `${request.body.location} has been added to your favorites` });
+            })
+            .catch(error => {
+              response.status(500).json({ error });
+            });
+
+
+        } else if (!request.body.location && user ){
+          response.status(400).json({error: 'Bad Request! Are you missing a location?'});
+        } else if (!user) {
+          response.status(401).json({error: 'Unauthorized!'});
+        }
+      }).catch(error => console.log(error));
+    } else {
+      response.status(400).json({error: 'Bad Request! Did you send an Api Key?'});
+    }
+});
+
+module.exports = router;
