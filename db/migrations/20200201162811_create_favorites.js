fee77c0dddcf4c4a7ea2c21a7a022ba87de19b13
Michael Cooper <macoop2363@gmail.com>
2020-02-02 10:04:23 -0700

    Added references to the Users table

--- db/migrations/20200201162811_create_favorites.js
+++ db/migrations/20200201162811_create_favorites.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('favorites', function(table) {
       table.increments('id').primary();
-      table.integer('user_id');
+      table.integer('user_id').references('users.id');
       table.string('location');
       table.float('lat')
       table.float('lng')
