cf02aac527ee23ea9646b197886bfe89a18bbb20
Laura Schulz <lrs8810@gmail.com>
2020-02-02 12:20:04 -0700

    Add Forecast pojo to format data

---
+++ pojos/forecast.js
@@ -0,0 +1,82 @@
+class Forecast {
+  constructor(location, data) {
+    this.location = location;
+    this.currently = this.formatCurrentWeather(data.currently);
+    this.hourly = this.formatHourly(data.hourly);
+    this.daily = this.formatDaily(data.daily);
+  }
+
+  formatCurrentWeather(data) {
+    return {
+      summary: data.summary,
+      icon: data.icon,
+      precipIntensity: data.precipIntensity,
+      precipProbability: data.precipProbability,
+      temperature: data.temperature,
+      humidity: data.humidity,
+      pressure: data.pressure,
+      windSpeed: data.windSpeed,
+      windGust: data.windGust,
+      windBearing: data.windBearing,
+      cloudCover: data.cloudCover,
+      visibility: data.visibility
+    }
+  }
+
+  formatHourly(data) {
+    return {
+      summary: data.summary,
+      icon: data.icon,
+      data: data.data.slice(0,8).map(hour => {
+        return {
+          time: hour.time,
+          summary: hour.summary,
+          icon: hour.icon,
+          precipIntensity: hour.precipIntensity,
+          precipProbability: hour.precipProbability,
+          temperature: hour.temperature,
+          humidity: hour.humidity,
+          pressure: hour.pressure,
+          windSpeed: hour.windSpeed,
+          windGust: hour.windGust,
+          windBearing: hour.windBearing,
+          cloudCover: hour.cloudCover,
+          visibility: hour.visibility
+        }
+      })
+    }
+  }
+
+  formatDaily(data) {
+    return {
+      summary: data.summary,
+      icon: data.icon,
+      data: data.data.slice(0,7).map(day => {
+        return {
+          time: day.time,
+          summary: day.summary,
+          icon: day.icon,
+          sunriseTime: day.sunriseTime,
+          sunsetTime: day.sunsetTime,
+          precipIntensity: day.precipIntensity,
+          precipIntensityMax: day.precipIntensityMax,
+          precipIntensityMaxTime: day.precipIntensityMaxTime,
+          precipProbability: day.precipProbability,
+          precipType: day.precipType,
+          temperatureHigh: day.temperatureHigh,
+          temperatureLow: day.temperatureLow,
+          humidity: day.humidity,
+          pressure: day.pressure,
+          windSpeed: day.windSpeed,
+          windGust: day.windGust,
+          cloudCover: day.cloudCover,
+          visibility: day.visibility,
+          temperatureMin: day.temperatureMin,
+          temperatureMax: day.temperatureMax
+        }
+      })
+    }
+  }
+}
+
+module.exports = Forecast
